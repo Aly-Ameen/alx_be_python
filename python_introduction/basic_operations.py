843af50711d74ff2d5ff917b8eec18b854e251f4
AlyAmeen <eng-ali.mohamed1520@alexuedu.eg>
2024-06-23 23:37:25 +0300

    NewCommit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1, number2 = 10, 5
+
+s = number1 + number2
+d = number1 - number2
+p = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {s}')
+print(f'Subtraction of {number1} and {number2} is {d}')
+print(f'Multiplication of {number1} and {number2} is {p}')
